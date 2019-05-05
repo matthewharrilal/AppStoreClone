e4d8db82a317742e3326c7e479df3e1d49242baa
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-05 16:47:43 -0700

    Initial Commit of the App Store Clone

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Matthew Harrilal on 5/5/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AppStoreCloneTests/AppStoreCloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  AppStoreCloneTests.swift
+//  AppStoreCloneTests
+//
+//  Created by Matthew Harrilal on 5/5/19.
+//  Copyright © 2019 Matthew Harrilal. All rights reserved.
+//
+
+import XCTest
+@testable import AppStoreClone
+
+class AppStoreCloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
