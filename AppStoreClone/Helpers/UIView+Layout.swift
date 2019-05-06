65599e72bb05318dbabcf3781ebcd1d0c888020c
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-06 11:30:39 -0700

    Making progress on the anchor function making parameters optional

--- AppStoreClone/Helpers/UIView+Layout.swift
+++ AppStoreClone/Helpers/UIView+Layout.swift
@@ -11,15 +11,33 @@ import UIKit
 
 
 extension UIView {
-    func anchor(top: NSLayoutYAxisAnchor, leading: NSLayoutXAxisAnchor, bottom: NSLayoutYAxisAnchor, trailing: NSLayoutXAxisAnchor, padding: UIEdgeInsets = .zero) {
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
         // Enables auto layout
         self.translatesAutoresizingMaskIntoConstraints = false
         
-        topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
-        bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding.bottom).isActive = true
-        leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
-        trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
         
         
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
 }
