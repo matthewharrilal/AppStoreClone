911864d70db2e87063f861110ab8a6d8248521ef
Matthew Harrilal <matthewharrilal@gmail.com>
2019-05-06 11:53:32 -0700

    Added extension method to constrain view in center and constrain its height and width produces abstraction and less code duplication

--- AppStoreClone/Helpers/UIView+Layout.swift
+++ AppStoreClone/Helpers/UIView+Layout.swift
@@ -47,7 +47,46 @@ extension UIView {
         
     }
     
-    func fillSuperview() {
-        anchor(top: superview?.topAnchor, leading: superview?.leadingAnchor, bottom: superview?.bottomAnchor, trailing: superview?.trailingAnchor)
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        // Enables autolayout
+        translatesAutoresizingMaskIntoConstraints = true
+        
+        if let superviewTopAnchor = superview?.topAnchor {
+            // Constrain the top anchor of the view that is currently calling this method
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right)
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom)
+        }
+    }
+    
+    func centerInSuperview(withSize size: CGSize = .zero) {
+        // Constrains elements to center and constrains the size configurations to the view's superview
+        translatesAutoresizingMaskIntoConstraints = true
+        
+        if let superviewCenterXAnchor = superview?.centerXAnchor {
+            centerXAnchor.constraint(equalTo: superviewCenterXAnchor).isActive = true
+        }
+        
+        if let superviewCenterYAnchor = superview?.centerYAnchor {
+            centerYAnchor.constraint(equalTo: superviewCenterYAnchor).isActive = true
+        }
+        
+        if size.width != 0 {
+            widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
     }
 }
